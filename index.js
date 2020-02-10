5c3994acea7fb2f9c01132ef7433ec0c11e11b84
张显磊 <1079105171@qq.com>
2020-02-10 15:14:46 +0800

    开始个人网站服务端建设

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿const http = require('http');
+
+const app = http.createServer();
+
+app.on('request', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf8'});
+    res.end(`<div>爱你宝贝！</div>`)
+})
+
+app.listen(3000);
+console.log('服务器已启动...')
